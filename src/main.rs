38bfdf91abcb44a0752244703f57dc95303a0edf
Obei Sideg <obei.sideg@gmail.com>
2024-04-26 15:06:50 +0300

    snapchat: Add functionality to delete users by email from a segment

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ async fn main() -> Result<()> {
     let res = client
         .user()
         .await
-        .add_users(
+        .delete_users(
             "6798030683466871".to_string(),
             vec!["g1@gmail.com".to_string(), "g2@gmail.com".to_string()],
         )
--- src/snapchat/user.rs
+++ src/snapchat/user.rs
@@ -41,4 +41,32 @@ impl<'a> UserService<'a> {
         }
         Ok(res.json().await?)
     }
+
+    /// Delete users by emails
+    pub async fn delete_users(
+        &self,
+        segment_id: String,
+        emails: Vec<String>,
+    ) -> Result<UsersResponse> {
+        let path = &format!("/segments/{segment_id}/users");
+        let mut data: Vec<Vec<String>> = Vec::new();
+        for email in emails.iter() {
+            let mut hasher = Sha256::new();
+            hasher.update(email);
+            let hashed_email = hasher.finalize();
+            let hex_data = hex::encode(hashed_email);
+            data.push(vec![hex_data]);
+        }
+        let body = serde_json::to_string(&UsersRequest {
+            users: vec![UserRequest {
+                schema: vec!["EMAIL_SHA256".to_string()],
+                data,
+            }],
+        })?;
+        let res = make_request(self.token, Method::DELETE, path, Some(body)).await?;
+        if !matches!(res.status(), StatusCode::OK) {
+            return Err(async_graphql::Error::new(res.text().await?));
+        }
+        Ok(res.json().await?)
+    }
 }
