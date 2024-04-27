4de86e3780a7548f1108d2c44848feb6754647f6
Obei Sideg <obei.sideg@gmail.com>
2024-04-27 16:32:07 +0300

    snapchat: Add module comment

--- src/snapchat/mod.rs
+++ src/snapchat/mod.rs
@@ -1,3 +1,7 @@
+//! The Snapchat module contains code that integrates with the Snapchat API.
+//!
+//! - ty: this submodule contains API model.
+
 pub mod client;
 pub mod request;
 pub mod segment;
