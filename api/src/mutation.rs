85ed3e6d0dcc4f009b94ff78fb49e5760a9041d9
Obei Sideg <obei.sideg@gmail.com>
2024-04-30 23:56:23 +0300

    api: Add API to add users for a segment
    Request Query sample:
    ```
    mutation {
        addUsers(segmentId: "4555810311682070", emails: ["g1@gmail.com", "g2@gmail.com"]) {
            request_status
            request_id
            users {
                sub_request_status
                user {
                    number_uploaded_users
                }
            }
        }
    }
    ```
    
    Response sample:
    ```
    mutation {
        addUsers(segmentId: "4555810311682070", emails: ["g1@gmail.com", "g2@gmail.com"]) {
            request_status
            request_id
            users {
                sub_request_status
                user {
                    number_uploaded_users
                }
            }
        }
    }
    ```

--- api/src/mutation.rs
+++ api/src/mutation.rs
@@ -3,6 +3,7 @@ use async_trait::async_trait;
 
 use snapchat::client::SnapChat;
 use snapchat::ty::segment::SegmentsResponse;
+use snapchat::ty::user::UsersResponse;
 
 use super::ty::get_app_info;
 
@@ -10,6 +11,13 @@ pub struct RootMutation;
 
 #[async_trait]
 trait Mutations {
+    async fn add_users(
+        &self,
+        ctx: &Context<'_>,
+        segment_id: String,
+        emails: Vec<String>,
+    ) -> Result<UsersResponse, Error>;
+
     async fn delete_all_users(
         &self,
         ctx: &Context<'_>,
@@ -20,6 +28,24 @@ trait Mutations {
 #[Object]
 #[async_trait]
 impl Mutations for RootMutation {
+    async fn add_users(
+        &self,
+        ctx: &Context<'_>,
+        segment_id: String,
+        emails: Vec<String>,
+    ) -> Result<UsersResponse, Error> {
+        let app = get_app_info(ctx)?;
+        let access_token = app.access_token.lock().await;
+        let client_id = app.client_id.lock().await;
+        let client_secret = app.client_secret.lock().await;
+        let refresh_token = app.refresh_token.lock().await;
+        let snapchat =
+            SnapChat::new(&access_token, &client_id, &client_secret, &refresh_token).await;
+        let user_client = snapchat.user().await;
+
+        Ok(user_client.add_users(segment_id, emails).await?)
+    }
+
     async fn delete_all_users(
         &self,
         ctx: &Context<'_>,
--- snapchat/src/ty/user.rs
+++ snapchat/src/ty/user.rs
@@ -1,30 +1,36 @@
+use async_graphql::SimpleObject;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, SimpleObject)]
+#[graphql(rename_fields = "snake_case")]
 pub struct UsersRequest {
     pub users: Vec<UserRequest>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, SimpleObject)]
+#[graphql(rename_fields = "snake_case")]
 pub struct UserRequest {
     pub schema: Vec<String>,
     pub data: Vec<Vec<String>>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, SimpleObject)]
+#[graphql(rename_fields = "snake_case")]
 pub struct UsersResponse {
     pub request_status: String,
     pub request_id: String,
     pub users: Vec<UserResponse>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, SimpleObject)]
+#[graphql(rename_fields = "snake_case")]
 pub struct UserResponse {
     pub sub_request_status: String,
     pub user: SubUserResponse,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, SimpleObject)]
+#[graphql(rename_fields = "snake_case")]
 pub struct SubUserResponse {
     pub number_uploaded_users: u32,
 }
