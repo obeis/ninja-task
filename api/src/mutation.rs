8ddfe043e3b7979d3c83e3527fe8da1d23a215de
Obei Sideg <obei.sideg@gmail.com>
2024-05-01 00:01:45 +0300

    api: Add mutation API to delete users from a segment
    Query:
    ```
    mutation {
        deleteUsers(segmentId: "", emails: ["", ""]) {
            request_status
            request_id
            users {
                sub_request_status
                user {
                    number_uploaded_users
                }
            }
        }
    }
    ```
    
    Response:
    ```
    {
        "data": {
            "deleteUsers": {
                "request_status": "SUCCESS",
                "request_id": "59da517e-a4c4-4aa7-9f1c-ed5379834edb",
                "users": [
                    {
                        "sub_request_status": "SUCCESS",
                        "user": {
                            "number_uploaded_users": 2
                        }
                    }
                ]
            }
        }
    }
    ```

--- api/src/mutation.rs
+++ api/src/mutation.rs
@@ -18,6 +18,13 @@ trait Mutations {
         emails: Vec<String>,
     ) -> Result<UsersResponse, Error>;
 
+    async fn delete_users(
+        &self,
+        ctx: &Context<'_>,
+        segment_id: String,
+        emails: Vec<String>,
+    ) -> Result<UsersResponse, Error>;
+
     async fn delete_all_users(
         &self,
         ctx: &Context<'_>,
@@ -44,6 +51,24 @@ impl Mutations for RootMutation {
         let user_client = snapchat.user().await;
 
         Ok(user_client.add_users(segment_id, emails).await?)
+    }
+
+    async fn delete_users(
+        &self,
+        ctx: &Context<'_>,
+        segment_id: String,
+        emails: Vec<String>,
+    ) -> Result<UsersResponse, Error> {
+        let app = get_app_info(ctx)?;
+        let access_token = app.access_token.lock().await;
+        let client_id = app.client_id.lock().await;
+        let client_secret = app.client_secret.lock().await;
+        let refresh_token = app.refresh_token.lock().await;
+        let snapchat =
+            SnapChat::new(&access_token, &client_id, &client_secret, &refresh_token).await;
+        let user_client = snapchat.user().await;
+
+        Ok(user_client.delete_users(segment_id, emails).await?)
     }
 
     async fn delete_all_users(
